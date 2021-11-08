0776e33ffb5680511e7658780c7942dcecb39ff0
tosery <83147935+tosery@users.noreply.github.com>
2021-11-08 20:07:54 +0900

    start

---
+++ app/src/main/java/com/team/project/comment/CommentModel.kt
@@ -0,0 +1,6 @@
+package com.team.project.comment
+
+data class CommentModel (
+    val commentTitle : String = "",
+    val commentCreatedTime : String = ""
+)
---
+++ app/src/main/java/com/team/project/contentsList/BookmarkModel.kt
@@ -0,0 +1,5 @@
+package com.team.project.contentsList
+
+data class BookmarkModel (
+    val bookmarkIsTrue : Boolean? = null
+)
