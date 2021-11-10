eda7bdba4ac0bdd0bb85a14e7ab55beac4c0b9f5
tosery <83147935+tosery@users.noreply.github.com>
2021-11-10 12:04:04 +0900

    캘린더(테스트 중)

---
+++ app/src/main/java/com/team/project/CalendarTodolist.kt
@@ -0,0 +1,88 @@
+package com.team.project
+
+
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.Calendar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+
+class CalendarTodolist : AppCompatActivity() {
+    lateinit var myRef : DatabaseReference
+    lateinit var dp : DatePicker
+    lateinit var edtDiary : EditText
+    lateinit var btnWrite : Button
+    lateinit var fileName : String
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar_todolist)
+        val database = Firebase.database
+        val todolist = intent.getStringExtra("totolist")
+        myRef = database.getReference("todolist")
+        title = "간단 일기장 (수정)"
+
+        dp = findViewById<DatePicker>(R.id.datePicker1)
+        edtDiary = findViewById<EditText>(R.id.edtDiary)
+        btnWrite = findViewById<Button>(R.id.btnWrite)
+
+        var cal = Calendar.getInstance()
+        var cYear = cal.get(Calendar.YEAR)
+        var cMonth = cal.get(Calendar.MONTH)
+        var cDay = cal.get(Calendar.DAY_OF_MONTH)
+
+        // 처음 실행시에 설정할 내용
+        fileName = (Integer.toString(cYear) + "_" + Integer.toString(cMonth + 1)
+                + "_" + Integer.toString(cDay) + ".txt")
+        var str = readDiary(fileName)
+        edtDiary.setText(str)
+
+        dp.init(cYear, cMonth, cDay) { view, year, monthOfYear, dayOfMonth ->
+            fileName = (Integer.toString(year) + "_"
+                    + Integer.toString(monthOfYear + 1) + "_"
+                    + Integer.toString(dayOfMonth) + ".txt")
+            var str = readDiary(fileName)
+            edtDiary.setText(str)
+        }
+
+        btnWrite.setOnClickListener {
+            var outFs = openFileOutput(fileName, Context.MODE_PRIVATE)
+            var str = edtDiary.text.toString()
+            outFs.write(str.toByteArray())
+            outFs.close()
+            Toast.makeText(applicationContext, "$fileName 이 저장됨", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    fun readDiary(fName: String) : String? {
+        var diaryStr : String? = null
+        var inFs: FileInputStream
+        try {
+            inFs = openFileInput(fName)
+            var txt = ByteArray(500)
+            inFs.read(txt)
+            inFs.close()
+            diaryStr = txt.toString(Charsets.UTF_8).trim()
+            btnWrite.text = "수정 하기"
+        } catch (e : IOException) {
+            edtDiary.hint = "일기 없음"
+            btnWrite.text = "새로 저장"
+        }
+
+        return diaryStr
+    }
+
+}
--- app/src/main/java/com/team/project/fragments/TipFragment.kt
+++ app/src/main/java/com/team/project/fragments/TipFragment.kt
@@ -1,16 +1,24 @@
 package com.team.project.fragments
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+
 import com.team.project.R
+import com.team.project.CalendarTodolist
 import com.team.project.contentsList.ContentListActivity
 import com.team.project.databinding.FragmentTipBinding
+import kotlinx.android.synthetic.main.fragment_tip.*
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
 
 class TipFragment : Fragment() {
 
@@ -18,6 +26,19 @@ class TipFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+/*        btn_calendar_write.setOnClickListener {
+            try {
+                //TODO:프래그먼트에선 openFileInput이 안되서 액티비티로 해야하나
+                var inFs : FileInputStream = openFileInput("file.txt")
+                var txt = ByteArray(30)
+                inFs.read(txt)
+                var str = txt.toString(Charsets.UTF_8)
+                Toast.makeText(applicationContext, str, Toast.LENGTH_SHORT).show()
+                inFs.close()
+            } catch (e : IOException) {
+                Toast.makeText(applicationContext, "파일 없음", Toast.LENGTH_SHORT).show()
+            }
+        }*/
 
     }
 
@@ -29,20 +50,31 @@ class TipFragment : Fragment() {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_tip, container, false)
 
-        binding.category1.setOnClickListener {
+        binding.btnSelectDate.setOnClickListener {
 
-            val intent = Intent(context, ContentListActivity::class.java)
-            intent.putExtra("category", "category1")
-            startActivity(intent)
+//            val intent = Intent(context, ContentListActivity::class.java)
+//            intent.putExtra("category", "category1")
+//            startActivity(intent)
+            // create new instance of DatePickerFragment
+            val datePickerFragment = DatePickerDiaryFragment()
+            val supportFragmentManager = requireActivity().supportFragmentManager
 
+            // we have to implement setFragmentResultListener
+            supportFragmentManager.setFragmentResultListener(
+                "REQUEST_KEY",
+                viewLifecycleOwner
+            ) { resultKey, bundle ->
+                if (resultKey == "REQUEST_KEY") {
+                    val date = bundle.getString("SELECTED_DATE")
+                    tvSelectedDate.text = date
+                }
+            }
+
+            // show
+            datePickerFragment.show(supportFragmentManager, "DatePickerFragment")
         }
 
-        binding.category2.setOnClickListener {
-            val intent = Intent(context, ContentListActivity::class.java)
-            intent.putExtra("category", "category2")
-            startActivity(intent)
 
-        }
 
         binding.homeTap.setOnClickListener {
             it.findNavController().navigate(R.id.action_tipFragment_to_homeFragment)
@@ -52,13 +84,21 @@ class TipFragment : Fragment() {
             it.findNavController().navigate(R.id.action_tipFragment_to_talkFragment)
         }
 
-        binding.bookmarkTap.setOnClickListener {
-            it.findNavController().navigate(R.id.action_tipFragment_to_bookmarkFragment)
-        }
 
         binding.storeTap.setOnClickListener {
             it.findNavController().navigate(R.id.action_tipFragment_to_storeFragment)
         }
+
+        binding.btnCalendarWrite.setOnClickListener{
+            val intent = Intent(context, CalendarTodolist::class.java)
+            intent.putExtra("todolist", "todolist")
+            startActivity(intent)
+        }
+/*        binding.btnCalendarWrite.setOnClickListener{
+            val intent = Intent(context, ContentListActivity::class.java)
+            intent.putExtra("todolist", "todolist")
+            startActivity(intent)
+        }//새로 액티비티 열려고 했는데 실패*/
 
         return binding.root
     }
